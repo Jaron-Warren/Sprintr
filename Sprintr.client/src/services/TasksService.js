42b3764fc62e27a552522fdf96b7901ab3ad0a95
Jaron-Warren <Jarwarren@gmail.com>
2021-08-12 17:26:58 -0600

    create task

--- Sprintr.client/src/services/TasksService.js
+++ Sprintr.client/src/services/TasksService.js
@@ -2,6 +2,11 @@ import { AppState } from '../AppState'
 import { api } from './AxiosService'
 
 class TasksService {
+  async createTask(rawtask) {
+    await api.createTask('api/tasks', rawtask)
+    await this.getAll()
+  }
+
   async destroyTask(id) {
     await api.delete('api/tasks' + id)
     AppState.tasks = AppState.tasks.filter(t => t.id !== id)
